e3f50c63249c4d24d6962373c7f27a39252a8652
shyam sundar yadav <shyamkryadav345@gmail.com>
2024-01-20 10:51:24 +0545

    user register scheam

---
+++ server/src/models/userRegister.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+
+const userSchema = new mongoose.Schema({
+  userName: String, 
+  email: String,
+  password: String,
+});
+
+const UserAccount = mongoose.model('UserRegister', userSchema);
+module.exports = UserAccount
