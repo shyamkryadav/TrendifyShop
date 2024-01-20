899dc35d9aa7cff433a00ab1fcb7ca7a47b3087f
shyam sundar yadav <shyamkryadav345@gmail.com>
2024-01-20 20:31:18 +0545

    footer is complete

---
+++ client/src/app/footer/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./page.css";
+const page = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 TrendyShop</p>
+    </footer>
+  );
+};
+
+export default page;
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./header/page";
+import Footer from "./footer/page";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Footer />
     </main>
   );
 }
