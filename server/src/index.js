8f486387da385a3b36d089d2fc2b116ceb8063b1
shyam sundar yadav <shyamkryadav345@gmail.com>
2024-01-20 10:52:27 +0545

    configure in index file

--- server/src/index.js
+++ server/src/index.js
@@ -5,11 +5,37 @@ const port = process.env.PORT;
 app.use(express.json());
 
 // data base connection 
+const connection = require('./db/connection')
+const userRegister = require('./models/userRegister');
+const userLogin = require('./models/userLogin');
+const Product = require('./models/product');
+const Category = require('./models/category');
+
+connection() // db connect
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.post('/register', async (req, res) => {
+    await userRegister.create(req.body);
 })
+    
+app.post('/login', async (req, res) => {
+    await userLogin.create(req.body);
+})
+app.post('/product', async (req, res) => {
+    await Product.create(req.body);
+})
+
+app.post('/category', async (req, res) => {
+    await Category.create(req.body);
+})
+    
+    
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+
 
 
 
